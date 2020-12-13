35e082cfd43d2d4f30144ae62fb21cc9ab1e674d
quawai <quawai@me.com>
2020-12-14 07:29:55 +0900

    solved (typo in definition of Ack-func.)

--- excercises/chapter1/1-010.scm
+++ excercises/chapter1/1-010.scm
@@ -6,7 +6,7 @@
   (cond ((= y 0) 0)
 	((= x 0) (* 2 y))
 	((= y 1) 2)
-        (else A (- x 1) (A x (- y 1)))))
+        (else (A (- x 1) (A x (- y 1))))))
 
 (display "(A 1 10) is equal to ")
 (A 1 10)
@@ -19,13 +19,13 @@
 (display "(A 4 2) is equal to ")
 (A 4 2)
 (display "(A 4 3) is equal to ")
-(A 4 3)
+;(A 4 3)
 (display "\n")
 
 
-(define (f n) (A 0 n))
-(define (g n) (A 1 n))
-(define (h n) (A 2 n))
+(define (f n) (A 0 n)) ;f(n) = 2*n
+(define (g n) (A 1 n)) ; g(n) = 2^n
+(define (h n) (A 2 n)) ; h(n) = hyper(2,4,n)
 (define (k n) (* 5 n n))
 
 (define (power-2 n)
@@ -38,10 +38,6 @@
       2
       (+ 2 (times-2 (- n 1)))))
 
-(define (F n) (times-2 n))
-(define (G n) (if (> n 0)
-                  2
-                  0))
 
 (define (f-0 n) (A n 0))
 (define (f-1 n) (A n 1))
@@ -54,16 +50,7 @@
 (f 3)
 (f 4)
 (f 5)
-(f 6)
-
-(display "results of F\n")
-
-(F 1)
-(F 2)
-(F 3)
-(F 4)
-(F 5)
-(F 6)
+;(f 6)
 
 (display "results of g\n")
 
@@ -72,25 +59,20 @@
 (g 3)
 (g 4)
 (g 5)
-(g 6)
+;(g 6)
 
-(display "results of G\n")
+(display "g(10)\n")
+(g 10)
 
-(G 1)
-(G 2)
-(G 3)
-(G 4)
-(G 5)
-(G 6)
-
+(display "\n")
 (display "results of h\n")
 
 (h 1)
 (h 2)
 (h 3)
 (h 4)
-(h 5)
-(h 6)
+;(h 5) ; too much large
+;(h 6)
 
 (display "results of f-0\n")
 
@@ -99,7 +81,7 @@
 (f-0 3)
 (f-0 4)
 (f-0 5)
-(f-0 6)
+;(f-0 6)
 
 (display "results of f-1\n")
 
@@ -108,7 +90,7 @@
 (f-1 3)
 (f-1 4)
 (f-1 5)
-(f-1 6)
+;(f-1 6)
 
 (display "results of f-2\n")
 
@@ -117,7 +99,7 @@
 (f-2 3)
 (f-2 4)
 (f-2 5)
-(f-2 6)
+;(f-2 6)
 
 (display "results of k\n")
 
@@ -127,4 +109,4 @@
 (k 3)
 (k 4)
 (k 5)
-(k 6)
+;(k 6)
